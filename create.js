63dc133bf4844af3413e81228b356bf73e5997e7
Jae <jae@zero5.co>
2020-06-07 18:51:48 +0900

    First Commit

--- create.js
+++ create.js
@@ -1,44 +1,29 @@
-import * as uuid from "uuid"
-import AWS from "aws-sdk"
+import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
-
   const params = {
-    TableName: process.env.TableName,
-
+    TableName: process.env.tableName,
+    // 'Item' contains the attributes of the item to be created
+    // - 'userId': user identities are federated through the
+    //             Cognito Identity Pool, we will use the identity id
+    //             as the user id of the authenticated user
+    // - 'noteId': a unique uuid
+    // - 'content': parsed from request body
+    // - 'attachment': parsed from request body
+    // - 'createdAt': current Unix timestamp
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: uuid.v1(),
       content: data.content,
-      attachment: data.atachment,
+      attachment: data.attachment,
       createdAt: Date.now()
     }
   };
 
-  dynamoDb.put(params, (error, data) => {
-    const headers = {
-      "Access-Control-Allow-Origin": "*"
-      "Access-Control-Allow-Credentials": true
-    };
+  await dynamoDb.put(params);
 
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false})
-      };
-      callback(null, response);
-      return;
-    }
-
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
-}
+  return params.Item;
+});
