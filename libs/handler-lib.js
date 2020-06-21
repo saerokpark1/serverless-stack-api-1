1a5a05b21db97c8a13888e00f9b0b8ac82b90c93
Jae <jae@zero5.co>
2020-06-21 00:29:51 -0700

    Setting up our Serverless infrastructure

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -15,7 +15,7 @@ export default function handler(lambda) {
         statusCode,
         headers: {
           "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Credentials": true
         },
         body: JSON.stringify(body),
       }));
